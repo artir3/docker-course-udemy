8bb4b1e4667944d263e23e9d97dd0b14db291059
artir3 <markowski.artur@outlook.com>
2019-11-11 23:54:14 +0100

    added to deploy

---
+++ README.md
@@ -0,0 +1,10 @@
+This repository was created when I was learning Docker and Kubernetes from Udemy course.
+
+https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide 
+
+Children repositories are 
+Section 7 -> https://github.com/artir3/docker-react
+Section 11 to 13 -> https://github.com/artir3/docker-multiconteiner
+
+Warning 
+There are some my changes from original code shown in the course.
--- section_14/deploy.sh
+++ section_14/deploy.sh
@@ -1,8 +1,21 @@
-docker build -t artir3/docker-course-client:latest -f ./client/Dockerfile ./client
-docker build -t artir3/docker-course-server:latest -f ./server/Dockerfile ./server
-docker build -t artir3/docker-course-worker:latest -f ./worker/Dockerfile ./worker
-docker push artir3/docker-course-client
-docker push artir3/docker-course-server
-docker push artir3/docker-course-worker
+docker build -t artir3/docker-course-client:latest \
+    -t artir3/docker-course-client:$SHA \
+    -f ./client/Dockerfile ./client
+docker build -t artir3/docker-course-server:latest \
+    -t artir3/docker-course-server:$SHA \
+    -f ./server/Dockerfile ./server
+docker build -t artir3/docker-course-worker:latest \
+    -t artir3/docker-course-worker:$SHA \
+    -f ./worker/Dockerfile ./worker
+
+docker push artir3/docker-course-client:latest
+docker push artir3/docker-course-client:$SHA
+docker push artir3/docker-course-server:latest
+docker push artir3/docker-course-server:$SHA
+docker push artir3/docker-course-worker:latest
+docker push artir3/docker-course-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=artir3/docker-course-server
+kubectl set image deployments/client-deployment server=artir3/docker-course-client:$SHA
+kubectl set image deployments/server-deployment server=artir3/docker-course-server:$SHA
+kubectl set image deployments/worker-deployment server=artir3/docker-course-worker:$SHA
