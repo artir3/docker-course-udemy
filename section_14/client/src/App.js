8f7460dcac77e0cc3bbcffa99d378786265c7e64
artir3 <markowski.artur@outlook.com>
2019-11-12 01:00:01 +0100

    added scaffold to project

--- section_14/client/src/App.js
+++ section_14/client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title">Wersja 1.0 on KUBERNETES</h1>
+            <h1 className="App-title">Wersja 1.0 on KUBERNETES and Skaffold</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
